353b0de014948fbc20be323eb65aa27037086e12
WanPeng <986063761@qq.com>
2020-10-10 15:50:12 +0800

    更新

--- src/components/VueBetterScroll/VueBetterScroll.vue
+++ src/components/VueBetterScroll/VueBetterScroll.vue
@@ -1,23 +1,26 @@
 <template>
-  <div class="vue-bs-wrapper">
+  <div class="vue-bs-wrapper" :style="style">
     <div ref="content" class="vue-bs-content">
+      <div class="placeholder" v-if="placeholderStyle" :style="placeholderStyle"></div>
       <slot></slot>
       <loading class="pull-up-slot" v-if="pullUp && startPullUp"></loading>
     </div>
 
-    <div class="pull-down-slot" v-if="pullDown">
+    <div class="pull-down-slot" v-if="pullDown && startPullDown">
       <img class="icon" 
         :class="{rotate: !downImgStyle && pullDowning}" 
-        :src="`${$rootPath}/static/favicon.ico`" 
+        :src="require('@/assets/img/refresh.svg')"
         :style="downImgStyle">
-      <span class="text" v-show="!pullDowning">刷新成功</span>
+      <span class="text" v-show="!pullDowning">
+        刷新成功
+      </span>
     </div>
   </div>
 </template>
 
 <script>
   import BScroll from 'better-scroll'
-  import {addResizeListener, removeResizeListener} from '@/assets/js/dom'
+  import {addResizeListener, removeResizeListener, getStyle} from '@/assets/js/dom'
   import { debounce } from 'vux'
 
   export default {
@@ -28,21 +31,43 @@
     },
     data() {
       return {
+        style: null,
         pullDowning: false,
         downImgStyle: null,
-        startPullUp: false
+        startPullUp: false,
+        startPullDown: false,
+        placeholderStyle: null
       }
     },
     methods: {
       resize: debounce(function () {
         this.refresh()
       }, 500),
+      setStyle(obj) {
+        this.style = obj
+      },
+      scrollBy(x, y) {
+        // 只有向上滚动,或者是有向下滚动的距离的时候才滚动,没有距离了就不滚动了
+        if (y < 0 || y > 0 && this.bs.y < 0 && y < (-this.bs.y)) {
+          this.bs.scrollBy(x, y)
+          this.placeholderStyle = null
+          return true
+        } else {
+          // 这里如果顶部没有地方可以滚动了,就垫一个相同高度的元素再滚动
+          this.placeholderStyle = y ? {
+            height: y + 'px',
+            background: '#fff'
+          } : null
+          return false
+        }
+      },
       // 下拉刷新后告诉组件已经完毕了，优化体验
       finishPullDown() {
-        this.pullDowning = false
-        this.downImgStyle = null
+        this.pullDowning = false;
+        this.downImgStyle = null;
         setTimeout(() => {
           this.bs.finishPullDown()
+          this.startPullDown = false
         }, 500)
       },
       // 上拉加载后告诉组件已经完毕了，优化体验
@@ -54,6 +79,19 @@
       refresh() {
         this.bs.refresh()
       },
+      // 矫正滚动状态
+      correctScrollStatus: debounce(function () {
+        // 如果滚动结束后滚动条没消失,组件会以为还在滚动中,导致无法点击,则立即停止当前滚动过程
+        if (getStyle(this.scrollbar).opacity === '1') {
+          // 这个方法是在源码中找到的设置 pointer-event 样式的方法,因为 pointer-event 被设置为了 none 导致无法点击
+          this.bs.scroller.togglePointerEvents(true)
+        }
+
+        // 滚动完成后一段时间还存在 startPullDown 则强制重置,避免下拉刷新的内容显示出来
+        setTimeout(() => {
+          this.startPullDown = false
+        }, 500)
+      }, 500),
       init() {
         let stop = 80
 
@@ -66,6 +104,9 @@
           preventDefaultException: {
             tagName: /^(DIV|SPAN|IMG|INPUT|TEXTAREA|BUTTON|SELECT|AUDIO)$/
           },
+          scrollbar: {
+            fade: true
+          },
           // 是否支持下拉刷新
           pullDownRefresh: this.pullDown ? {
             stop
@@ -75,31 +116,10 @@
             threshold: 20
           } : false
         })
+        const hooks = this.bs.scroller.actionsHandler.hooks
 
         // 下拉加载
         if (this.pullDown) {
-          this.bs.on('scroll', position => {
-            if (position.y > 0) {
-              if (this.emitPullDown) {
-                this.downImgStyle = null
-                return
-              }
-              
-              this.pullDowning = true
-              this.downImgStyle = {
-                top: `${position.y / 3}px`,
-                transform: `rotate(${position.y}deg) `
-              }
-            } else {
-              this.pullDowning = false
-              this.downImgStyle = null
-            }
-          })
-
-          this.bs.on('scrollEnd', () => {
-            this.emitPullDown = false
-          })
-          
           this.bs.on('pullingDown', event => {
             this.downImgStyle = null
             this.emitPullDown = true
@@ -107,16 +127,61 @@
           })
         }
 
+        hooks.on('move', event => {
+          if (this.pullDown) {
+            // 如果手指向下滑动
+            if (this.bs.movingDirectionY < 0) {
+              this.startPullDown = true
+            }
+          }
+        })
+
+        this.bs.on('scroll', position => {
+          if (this.pullDown) {
+            if (position.y > 0) {
+              if (this.emitPullDown) {
+                this.downImgStyle = null
+                return
+              }
+
+              this.pullDowning = true
+              this.downImgStyle = {
+                top: `${position.y / 3}px`,
+                transform: `rotate(${position.y}deg)`
+              }
+            } else {
+              this.pullDowning = false
+              this.downImgStyle = null
+            }
+            this.$emit('scroll', position.y)
+          }
+
+          // 滚动后不断触发滚动状态校验
+          this.correctScrollStatus()
+        })
+
+        this.bs.on('scrollEnd', () => {
+          if (this.pullDown) {
+            this.emitPullDown = false
+            this.startPullDown = false
+          }
+        })
+
         // 上拉加载
         if (this.pullUp) {
           this.bs.on('pullingUp', event => {
             this.startPullUp = true
             this.$emit('pull-up')
-
           })
         }
 
         addResizeListener(this.$refs.content, this.resize)
+
+        // 初始化完成后，触发一次刷新
+        this.$nextTick(() => {
+          this.scrollbar = this.bs.wrapper.querySelector('.bscroll-vertical-scrollbar')
+          this.refresh()
+        })
       }
     },
     beforeDestroy() {
@@ -133,6 +198,7 @@
   .vue-bs-wrapper {
     position: relative;
     overflow: hidden;
+    touch-action: pan-y;
 
     @keyframes rotate {
       from {
