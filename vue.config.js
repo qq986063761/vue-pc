9de9b4ab480bb43f174f4015d7884a45e7f4f9a9
WanPeng <986063761@qq.com>
2021-01-22 11:16:18 +0800

    更新

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
+const { ESBuildPlugin } = require('esbuild-loader')
+
 // 覆盖 webpack 配置
 module.exports = {
   // 路径配置
@@ -5,7 +7,20 @@ module.exports = {
   publicPath: './',
   // sourcemap
   productionSourceMap: false,
+  configureWebpack: {
+    plugins: [
+      new ESBuildPlugin()
+    ]
+  },
   chainWebpack: config => {
+    // esbuild 配置
+    config.module
+      .rule('js')
+      .test('/.js$/')
+      .use('esbuild-loader')
+      .loader('esbuild-loader')
+      .end()
+
     const oneOfsMap = config.module.rule('scss').oneOfs.store
     oneOfsMap.forEach(item => {
       item
